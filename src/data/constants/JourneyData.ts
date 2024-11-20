36b576b1402e601f0034cae0a6c628d79137e0cc
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-20 17:12:44 -0300

    feat: journey data

---
+++ src/data/constants/JourneyData.ts
@@ -0,0 +1,24 @@
+import { JourneyModel } from '../models/JourneyModel'
+
+export const JourneyData: JourneyModel[] = [
+  {
+    id: 1,
+    company: 'Agência de tecnologia do estado (ATI)',
+    role: 'Desenolvedor Fullstack',
+    roleType: 'Estágio',
+    startedIn: 'Setembro de 2023',
+    endedIn: 'Setembro de 2024',
+    description:
+      'Recebia demandas das secretarias do Estado para resolver questões relacionadas a inscrições e votações de programas desenvolvidos pelo governo do Maranhão, utilizando HTML, CSS, JavaScript, PHP e MySQL/MariaDB. Esse trabalho não apenas contribuiu para o meu crescimento profissional, mas também na resolução de problemas para as secretárias do estado.',
+  },
+  {
+    id: 2,
+    company: 'Dia de obra',
+    role: 'Desenvolvedor Fullstack',
+    roleType: 'Estágio',
+    startedIn: 'Maio de 2023',
+    endedIn: 'Setembro de 2023',
+    description:
+      'Trabalhei na implementação de novas funcionalidades e na correção de bugs de uma plataforma já existente, construída com Next.js, NestJS e Postgres. Essa plataforma tinha como missão facilitara elaboração de relatórios diários das obras realizadas pelos engenheiros, promovendo uma gestãomais eficiente e uma apresentação mais atraente para os clientes. Com essas melhorias, conseguimosgarantir funcionalidades essenciais para o aumento do número de clientes do sistema.',
+  },
+]
