74b6ad60944ee56c523e4beb608ee5e623a1744a
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-20 13:37:39 -0300

    feat: initial journey section

---
+++ src/components/Journey.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from './SectionTitle'
+
+export function Journey() {
+  return (
+    <section id="journey" className="min-h-screen">
+      <SectionTitle title="Minha jornada" />
+    </section>
+  )
+}
