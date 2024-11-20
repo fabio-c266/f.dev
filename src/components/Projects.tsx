f1c6ce2374b1893053e4ee66a21ae35c72c3138f
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-20 17:12:35 -0300

    feat: initial projects section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from './SectionTitle'
+
+export function Projects() {
+  return (
+    <section className="min-h-screen">
+      <SectionTitle title="Projetos" />
+    </section>
+  )
+}
