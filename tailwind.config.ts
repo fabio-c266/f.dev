98277ea07a67adb1a1ccf11c189b06b278c6ba74
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-20 13:26:03 -0300

    feat: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'black-100': '#000',
+        'gray-300': '#333',
+        'green-100': '#CAEBCF',
+        'green-500': '#2ECC71',
+        'white-400': '#f4f4f4',
+        'white-900': '#f9f9f9',
+      },
+      fontFamily: {
+        baloo_bhai_2: 'BalooBhai2, sans-serif',
+        raleway: 'Raleway, sans-serif',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
