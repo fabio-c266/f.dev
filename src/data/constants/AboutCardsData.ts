67f254b711df7e260057f827c7fadedb4b77e376
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-20 13:36:33 -0300

    feat: about me cards data

---
+++ src/data/constants/AboutCardsData.ts
@@ -0,0 +1,34 @@
+import { AboutCardModel } from '../models/AboutMeCardModel'
+
+export const AboutCardsData: AboutCardModel[] = [
+  {
+    id: 1,
+    img: {
+      name: 'Mão',
+      path: '/hand.png',
+    },
+    title: 'Introdução',
+    description:
+      'Estou nessa jornada deis dos meus 15 anos. Hoje, transformo ideias em soluções completas e funcionais',
+  },
+  {
+    id: 2,
+    img: {
+      name: 'Computador',
+      path: '/computer.png',
+    },
+    title: 'Minha experiência',
+    description:
+      'Com mais de 1 ano de experiência em desenvolvimento web, atuo como desenvolvedor fullstack, criando aplicações performáticas e atraentes. No frontend, utilizo React e Next.js, enquanto no backend, trabalho com Spring Boot e Laravel.',
+  },
+  {
+    id: 3,
+    img: {
+      name: 'Target',
+      path: '/target.png',
+    },
+    title: 'Objetivos',
+    description:
+      'Continuar aprendendo e explorando novas tecnologias e ferramentas, buscando constantemente o aprimoramento profissional para construir uma carreira de sucesso, impactante e repleta de realizações.',
+  },
+]
